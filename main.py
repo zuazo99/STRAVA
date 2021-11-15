bf38e69cf8b77b5dbdd936dd57f76926e0c0c0a2
olaba29 <andoni.ola@gmail.com>
2021-11-15 11:29:18 +0100

    Probak terminalean

--- main.py
+++ main.py
@@ -3,8 +3,23 @@ from controllers.StravaAPI import stravaApiKud
 
 
 if __name__ == '__main__':
+    print(" ")
+    print("Konexioa egiten APIarekin: ")
+    print(" ")
     stravaApiKud.getAccessToTheAPI()
+    print(" ")
+    print("1. Get Athlete: ")
+    print(" ")
     print(stravaApiKud.getAthlete())
+    print(" ")
+    print("1. Get AthleteActivities: ")
+    print(" ")
     print(stravaApiKud.getAthleteActivities())
+    print(" ")
+    print("1. Get ActivityById: ")
+    print(" ")
     print(stravaApiKud.getActivietesById())
+    print(" ")
+    print("1. Get ActivityStreams: ")
+    print(" ")
     print(stravaApiKud.getActivityStreams())
