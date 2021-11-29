cd7512e152c984d2b1ed5bf4bd55912282ff7060
zuazo99 <gzuazo63@gmail.com>
2021-11-29 09:30:35 +0100

    Klasea sortzen eta kodea antolatzen eta garbitzen

--- controllers/DBKud.py
+++ controllers/DBKud.py
@@ -1,39 +1,20 @@
 import sqlite3
 
 
-#class DBKudeaketa:
+class DBKudeaketa:
 
-    def datuBaseKud():
+    def datuBaseKonexioa(self):
         try:
             sqliteConnection = sqlite3.connect('AktibitateaInfo.db')
-            cursor = sqliteConnection.cursor()
+            #cursor = sqliteConnection.cursor()
             print("Konexioa ondo")
             #hemen sql kontsultak exekutatuko dira
 
-            sqlite_select_kontsulta ="select sqlite_version();"
-            cursor.execute(sqlite_select_kontsulta)
-            erantzuna = cursor.fetchall()
-
-            #query= "INSERT INTO Erabiltzailea(erabID, izena, abizena) VALUES(?,?,?)"
-            #cursor.execute(query, [id, izena, abizena])
-            #sqliteConnection.commit()
-            #print("Ondo gordeta")
-            query2= "SELECT * FROM Erabiltzailea;"
-            cursor.execute(query2)
-            erabiltzailea = cursor.fetchall()
-            #https://www.w3schools.com/python/ref_string_format.asp
-            print("+{:-<20}+{:-<20}+{:-<20}+".format("", "", ""))
-            print("|{:^20}|{:^20}|{:^20}|".format("erabID", "izena", "abizena"))
-            print("+{:-<20}+{:-<20}+{:-<20}+".format("", "", ""))
-
-            for erabID, izena, abizena in erabiltzailea:
-                print("|{:^20}|{:^20}|{:^20}|".format(erabID, izena, abizena))
-
-            print("+{:-<20}+{:-<20}+{:-<20}+".format("", "", ""))
-
-
-
-            cursor.close()
+            #sqlite_select_kontsulta ="select sqlite_version();"
+            #cursor.execute(sqlite_select_kontsulta)
+            #erantzuna = cursor.fetchall()
+            #print(erantzuna)
+            return sqliteConnection
 
 
         except sqlite3.Error as error:
@@ -44,4 +25,27 @@ import sqlite3
                 sqliteConnection.close()
                 print("SQLite konexioa itzi egin da")
 
+    def atletaSartu(self, datuak):
+        konexioa=self.datuBaseKonexioa()
+        cursor = konexioa.cursor()
+        query= "INSERT INTO Erabiltzailea(erabID, izena, abizena) VALUES(?,?,?)"
+        cursor.execute(query, [id, izena, abizena])
+        konexioa.commit()
+        cursor.close()
+        print("Ondo gordeta")
 
+    def atletaKontsultatu(self):
+        konexioa = self.datuBaseKonexioa()
+        cursor = konexioa.cursor()
+        query = "SELECT * FROM Erabiltzailea;"
+        cursor.execute(query)
+        erabiltzailea = cursor.fetchall()
+        # https://www.w3schools.com/python/ref_string_format.asp
+        print("+{:-<20}+{:-<20}+{:-<20}+".format("", "", ""))
+        print("|{:^20}|{:^20}|{:^20}|".format("erabID", "izena", "abizena"))
+        print("+{:-<20}+{:-<20}+{:-<20}+".format("", "", ""))
+
+        for erabID, izena, abizena in erabiltzailea:
+            print("|{:^20}|{:^20}|{:^20}|".format(erabID, izena, abizena))
+
+        print("+{:-<20}+{:-<20}+{:-<20}+".format("", "", ""))
