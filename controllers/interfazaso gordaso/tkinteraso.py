ec40a777fcfa4705699219bd27413d88fc759e8c
gorka <gorkadra6193@durangojesuitak.org>
2021-11-29 11:04:33 +0100

    aaaaaah windows32

--- controllers/interfazaso gordaso/tkinteraso.py
+++ controllers/interfazaso gordaso/tkinteraso.py
@@ -10,10 +10,36 @@ import controllers.DBKud as db
 class pantallitas:
     def __init__(self):
         self.datuak=db.DBKudeaketa()
+        print("pasa de datuak")
         self.window=tk.Tk()
-        self.window.geometry('500x400')
         self.window.title("STRAVass")
-        self.testua = tk.Label(self.window, text="gaur txalupen etorri gea")
-        self.testua.pack()
+        self.cuaderno=ttk.Notebook(self.window)
+        print("entra login")
+        self.login()
+        print("sale de login")
+        self.cuaderno.grid(column=0, row=0, padx=10, pady=10)
         self.window.mainloop()
-    #def login(self):
+        print("aaaaah")
+    def login(self):
+        print("aaaaah")
+        self.pagina1 = ttk.Frame(self.cuaderno)
+        self.cuaderno.add(self.pagina1, text="Carga de artículos")
+        self.labelframe1 = ttk.LabelFrame(self.pagina1, text="Artículo")
+        self.labelframe1.grid(column=0, row=0, padx=5, pady=10)
+        self.label1 = ttk.Label(self.labelframe1, text="Descripción:")
+        self.label1.grid(column=0, row=0, padx=4, pady=4)
+        self.descripcioncarga = tk.StringVar()
+        self.entrydescripcion = ttk.Entry(self.labelframe1, textvariable=self.descripcioncarga)
+        self.entrydescripcion.grid(column=1, row=0, padx=4, pady=4)
+        self.label2 = ttk.Label(self.labelframe1, text="Precio:")
+        self.label2.grid(column=0, row=1, padx=4, pady=4)
+        self.preciocarga = tk.StringVar()
+        self.entryprecio = ttk.Entry(self.labelframe1, textvariable=self.preciocarga)
+        self.entryprecio.grid(column=1, row=1, padx=4, pady=4)
+        self.boton1 = ttk.Button(self.labelframe1, text="Confirmar", command=self.agregar)
+        self.boton1.grid(column=1, row=2, padx=4, pady=4)
+
+    def agregar(self):
+        print("aaaaah")
+
+pantallitas()
